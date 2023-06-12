0764a3c293141554593c2eb1edaf8db9d7462af4
CaroChoch <fleur-a-fleur59@hotmail.fr>
2023-06-12 07:49:44 -0700

    remove media/photos/products

--- Dream_shop/dream_shop/views.py
+++ Dream_shop/dream_shop/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from store.models import Product, Category
+
 
 def home(request):
-  return render(request, 'home.html')
+    """
+    Vue pour afficher la page d'accueil du site.
+    """
+
+    # Récupérer tous les produits disponibles
+    category = Category.objects.get(category_name="Best seller")
+    products = Product.objects.filter(is_available=True, category=category)[:3]
+
+    # Préparer le contexte avec les produits pour l'affichage dans le template
+    context = {
+        'products': products,
+    }
+
+    # Renvoyer la réponse avec le template 'home.html' et le contexte
+    return render(request, 'home.html', context)
