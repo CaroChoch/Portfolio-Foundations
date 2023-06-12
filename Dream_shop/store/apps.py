d74bea3df291c845d0bb18a0b3e3bb49016560fe
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2023-06-12 12:56:11 -0700

    stcok local file display product store

--- Dream_shop/store/apps.py
+++ Dream_shop/store/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class StoreConfig(AppConfig):
+    """
+    Classe de configuration pour l'application 'store'.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
--- Dream_shop/store/views.py
+++ Dream_shop/store/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def store(request):
-  return render(request, 'store/store.html')
+  """
+  Vue pour afficher la page principale du magasin.
+  """
+  products = Product.objects.all().filter(is_available=True) # Récupère tous les produits disponibles
+
+  product_count = products.count() # Compte le nombre de produits disponibles
+
+  # Créer le contexte avec les produits et le nombre de produits
+  context = {
+    'products': products,
+    'product_count': product_count,
+  }
+  return render(request, 'store/store.html', context)
