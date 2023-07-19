7074d89b0ac8c1599e8762cdfb5d2f759734ea37
Mathieu Morel <113856302+MathieuMorel62@users.noreply.github.com>
2023-07-19 21:31:10 +0200

    Update comment for admin.py

--- Dream_shop/store/admin.py
+++ Dream_shop/store/admin.py
@@ -4,11 +4,11 @@ from .models import Product, Variation
 
 class ProductAdmin(admin.ModelAdmin):
 	"""
-	Admin configuration for the Product model.
-
-	Attributes:
-		list_display (tuple): Fields to display in the admin list view.
-		prepopulated_fields (dict): Fields to prepopulate based on other fields.
+		Admin configuration for the Product model.
+	
+		Attributes:
+			list_display (tuple): Fields to display in the admin list view.
+			prepopulated_fields (dict): Fields to prepopulate based on other fields.
 	"""
 	list_display = ('product_name', 'price', 'stock', 'category', 'modified_date', 'is_available')
 	prepopulated_fields = {'slug': ('product_name',)}
@@ -16,13 +16,13 @@ class ProductAdmin(admin.ModelAdmin):
 
 class VariationAdmin(admin.ModelAdmin):
 	"""
-    Admin configuration for the Variation model.
-
-    Attributes:
-      list_display (tuple): Fields to display in the admin list view.
-      list_editable (tuple): Fields that can be edited directly in the list view.
-      list_filter (tuple): Fields to use for filtering in the admin list view.
-  """
+	    Admin configuration for the Variation model.
+	
+	    Attributes:
+	      list_display (tuple): Fields to display in the admin list view.
+	      list_editable (tuple): Fields that can be edited directly in the list view.
+	      list_filter (tuple): Fields to use for filtering in the admin list view.
+  	"""
 	list_display = ('product', 'variation_category', 'variation_value', 'is_active')
 	list_editable = ('is_active',)
 	list_filter = ('product', 'variation_category', 'variation_value')
